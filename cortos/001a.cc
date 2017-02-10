caca708dfc0dd491e7f3a0cea9ed75246254ad0d
Nomar Mora <nomarmora@gmail.com>
2017-02-10 17:58:18 -0400

    Agregado codigos cortos
    001a prueba que cuando se hace sobrecarga de metodos se ejecuta el metodo de la clase base.
    001b haciendo uso de virtual ahora se ejecuta el metodo correcto.

---
+++ cortos/001a.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+ 
+class Persona {
+  public:
+   Persona(char *n) { strcpy(nombre, n); }
+   void VerNombre() { cout << nombre << endl; }
+  protected:
+   char nombre[30];
+};
+
+class Empleado : public Persona {
+  public:
+   Empleado(char *n) : Persona(n) {}
+   void VerNombre() { 
+      cout << "Emp: " << nombre << endl; 
+   }
+};
+
+class Estudiante : public Persona {
+  public:
+   Estudiante(char *n) : Persona(n) {}
+   void VerNombre() { 
+      cout << "Est: " << nombre << endl; 
+   }
+};
+
+int main() {
+   Persona *Pepito = new Estudiante("Jose");
+   Persona *Carlos = new Empleado("Carlos");
+
+   Carlos->VerNombre();
+   Pepito->VerNombre();
+   delete Pepito;
+   delete Carlos;
+   
+   return 0;
+}
